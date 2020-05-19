77cc67a38135885c11e9617fefbb35955232e209
aksoy <aksoy.android@gmail.com>
2020-05-19 10:57:43 +0300

    before polishing the ui

---
+++ app/src/main/java/com/arel/havadansudan/data/SunshinePreferences.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.arel.havadansudan.data;
+
+import android.content.Context;
+
+public class SunshinePreferences {
+
+
+    public static final String PREF_CITY_NAME = "city_name";
+
+    public static final String PREF_COORD_LAT = "coord_lat";
+    public static final String PREF_COORD_LONG = "coord_long";
+
+    private static final String DEFAULT_WEATHER_LOCATION = "94043,USA";
+    private static final double[] DEFAULT_WEATHER_COORDINATES = {37.4284, 122.0724};
+
+    private static final String DEFAULT_MAP_LOCATION =
+            "1600 Amphitheatre Parkway, Mountain View, CA 94043";
+
+    static public void setLocationDetails(Context c, String cityName, double lat, double lon) {
+
+    }
+
+    static public void setLocation(Context c, String locationSetting, double lat, double lon) {
+        /** This will be implemented in a future lesson **/
+    }
+
+    static public void resetLocationCoordinates(Context c) {
+        /** This will be implemented in a future lesson **/
+    }
+
+
+    public static String getPreferredWeatherLocation(Context context) {
+        /** This will be implemented in a future lesson **/
+        return getDefaultWeatherLocation();
+    }
+
+    public static boolean isMetric(Context context) {
+        /** This will be implemented in a future lesson **/
+        return true;
+    }
+
+    public static double[] getLocationCoordinates(Context context) {
+        return getDefaultWeatherCoordinates();
+    }
+
+    public static boolean isLocationLatLonAvailable(Context context) {
+        /** This will be implemented in a future lesson **/
+        return false;
+    }
+
+    private static String getDefaultWeatherLocation() {
+        /** This will be implemented in a future lesson **/
+        return DEFAULT_WEATHER_LOCATION;
+    }
+
+    public static double[] getDefaultWeatherCoordinates() {
+        /** This will be implemented in a future lesson **/
+        return DEFAULT_WEATHER_COORDINATES;
+    }
+}
--- app/src/main/java/com/arel/havadansudan/utils/Utils.java
+++
@@ -1,5 +0,0 @@
-package com.arel.havadansudan.utils;
-
-public class Utils {
-
-}
